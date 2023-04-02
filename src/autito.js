d52b17d2f940993e1f275809b4062443caba4d7a
Adriel <adriel.mounzon@ucb.edu.bo>
2023-04-01 21:48:27 -0400

    Muestra el mensaje inicial

--- src/autito.js
+++ src/autito.js
@@ -6,4 +6,8 @@ function mostrarControles() {
   return "<p> Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>";
 }
 
-export {mostrarInstrucciones, mostrarControles};
+function mostrarMensajeInicial() {
+  return "<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación, Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>";
+}
+
+export {mostrarInstrucciones, mostrarControles, mostrarMensajeInicial};
--- src/autito.test.js
+++ src/autito.test.js
@@ -1,4 +1,4 @@
-import {mostrarInstrucciones, mostrarControles} from "./autito.js";
+import {mostrarInstrucciones, mostrarControles, mostrarMensajeInicial} from "./autito.js";
 
 describe("Controlador de autito", () => {
   it("deberia mostrar el mensaje de instrucciones", () => {
@@ -7,5 +7,9 @@ describe("Controlador de autito", () => {
 
   it("deberia mostrar el mensaje de controles", () => {
     expect(mostrarControles()).toEqual("<p> Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>");
+  });
+
+  it("deberia mostrar el mensaje inicial", () => {
+    expect(mostrarInstrucciones()).toEqual("<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación </p>");
   });
 });
