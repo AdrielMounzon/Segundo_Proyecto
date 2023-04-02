5637b86843866e4002cebb822a9bb1d6cea0e60b
Adriel <adriel.mounzon@ucb.edu.bo>
2023-04-01 21:45:21 -0400

    Muestra el mensaje de los controles

--- src/autito.js
+++ src/autito.js
@@ -2,4 +2,8 @@ function mostrarInstrucciones() {
   return "<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación </p>";
 }
 
-export default mostrarInstrucciones;
+function mostrarControles() {
+  return "<p> Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>";
+}
+
+export {mostrarInstrucciones, mostrarControles};
--- src/autito.test.js
+++ src/autito.test.js
@@ -1,7 +1,11 @@
-import mostrarInstrucciones from "./autito.js";
+import {mostrarInstrucciones, mostrarControles} from "./autito.js";
 
 describe("Controlador de autito", () => {
   it("deberia mostrar el mensaje de instrucciones", () => {
     expect(mostrarInstrucciones()).toEqual("<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación </p>");
+  });
+
+  it("deberia mostrar el mensaje de controles", () => {
+    expect(mostrarControles()).toEqual("<p> Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>");
   });
 });
