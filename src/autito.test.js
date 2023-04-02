c45555fe90e9ab4e8cf77bf20d75bc2d798178cc
Adriel <adriel.mounzon@ucb.edu.bo>
2023-04-01 21:49:59 -0400

    Refactorización y correción de los mensajes

--- src/autito.test.js
+++ src/autito.test.js
@@ -2,14 +2,14 @@ import {mostrarInstrucciones, mostrarControles, mostrarMensajeInicial} from "./a
 
 describe("Controlador de autito", () => {
   it("deberia mostrar el mensaje de instrucciones", () => {
-    expect(mostrarInstrucciones()).toEqual("<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación </p>");
+    expect(mostrarInstrucciones()).toEqual("Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación");
   });
 
   it("deberia mostrar el mensaje de controles", () => {
-    expect(mostrarControles()).toEqual("<p> Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A' </p>");
+    expect(mostrarControles()).toEqual("Puedes girar el autito a la izquierda con la tecla 'I' y hacia la derecha con la tecla 'D', para avanzar usa la tecla 'A'");
   });
 
   it("deberia mostrar el mensaje inicial", () => {
-    expect(mostrarInstrucciones()).toEqual("<p> Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación </p>");
+    expect(mostrarInstrucciones()).toEqual("Debes configurar el tamaño del tablero, luego debes ingresar la posición incial del auto junto con su orientación, luego puedes introducir un comando para generar el movimiento, se mostrará la posición final del auto y su orientación");
   });
 });
